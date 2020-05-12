81a1004d7efcd349cf0ee0d2b071216bc3144d93
ossi <ossi@asgard.localdomain>
2020-05-12 22:53:00 +0200

    Introduce shared_examples_for_keep_until_services

--- spec/expire/calculate_first_to_keep_until_service_spec.rb
+++ spec/expire/calculate_first_to_keep_until_service_spec.rb
@@ -1,46 +1,36 @@
 # frozen_string_literal: true
 
 require 'test_dates'
+require 'support/shared_examples_for_keep_until_services'
 
 RSpec.describe Expire::CalculateFirstToKeepUntilService do
   describe 'hourly_to_keep_for' do
-    let(:backups) do
-      TestDates.create(hours: 9..12, minutes: (1..59).step(10))
-               .to_audited_backup_list
-    end
-
-    let(:rules) { Expire::Rules.new(hourly_to_keep_for: '3 hours') }
-
-    before do
-      described_class.call(backups, rules, DateTime.new(1860, 5, 17, 13, 51, 0))
-    end
-
-    it 'marks the expected backups as kept' do
-      expect(backups.filter(&:keep?)).to contain_exactly(
-        Expire::Backup.new(DateTime.new(1860, 5, 17, 12, 51, 0)),
-        Expire::Backup.new(DateTime.new(1860, 5, 17, 11, 51, 0)),
-        Expire::Backup.new(DateTime.new(1860, 5, 17, 10, 51, 0))
-      )
+    it_behaves_like 'a keep until service' do
+      let(:now) { DateTime.new(1860, 5, 17, 13, 51, 0) }
+      let(:build_rules_with) { { hourly_to_keep_for: '3 hours' } }
+      let(:build_backups_with) { { hours: 9..12, minutes: (1..59).step(10) } }
+      let(:expected_backups) do
+        [
+          Expire::Backup.new(DateTime.new(1860, 5, 17, 12, 51, 0)),
+          Expire::Backup.new(DateTime.new(1860, 5, 17, 11, 51, 0)),
+          Expire::Backup.new(DateTime.new(1860, 5, 17, 10, 51, 0))
+        ]
+      end
     end
   end
 
   describe 'yearly_to_keep_for' do
-    let(:backups) do
-      TestDates.create(years: 1857..1860).to_audited_backup_list
-    end
-
-    let(:rules) { Expire::Rules.new(yearly_to_keep_for: '2 years') }
-
-    before do
-      described_class.call(backups, rules, DateTime.new(1860, 1, 1, 12, 0, 0))
-    end
-
-    it 'marks the expected backups as kept' do
-      expect(backups.filter(&:keep?)).to contain_exactly(
-        Expire::Backup.new(DateTime.new(1860, 5, 17, 12, 0, 0)),
-        Expire::Backup.new(DateTime.new(1859, 5, 17, 12, 0, 0)),
-        Expire::Backup.new(DateTime.new(1858, 5, 17, 12, 0, 0))
-      )
+    it_behaves_like 'a keep until service' do
+      let(:now) { DateTime.new(1860, 1, 1, 12, 0, 0) }
+      let(:build_rules_with) { { yearly_to_keep_for: '2 years' } }
+      let(:build_backups_with) { { years: 1857..1860 } }
+      let(:expected_backups) do
+        [
+          Expire::Backup.new(DateTime.new(1860, 5, 17, 12, 0, 0)),
+          Expire::Backup.new(DateTime.new(1859, 5, 17, 12, 0, 0)),
+          Expire::Backup.new(DateTime.new(1858, 5, 17, 12, 0, 0))
+        ]
+      end
     end
   end
 end
---
+++ spec/support/shared_examples_for_keep_until_services.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'a keep until service' do
+  it 'marks the expected backups to keep' do
+    backups = TestDates.create(build_backups_with).to_audited_backup_list
+    rules   = Expire::Rules.new(build_rules_with)
+
+    described_class.call(backups, rules, now)
+
+    # Note the splat operator
+    expect(backups.filter(&:keep?)).to contain_exactly(*expected_backups)
+  end
+end

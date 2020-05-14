a3ec754181acf74850966ef340eabba67d670167
ossi <ossi@asgard.localdomain>
2020-05-14 20:33:25 +0200

    Rm STEP_WITHS-loop from KeepFirstOfIntervalUntil...

--- lib/expire/keep_first_of_interval_until_service.rb
+++ lib/expire/keep_first_of_interval_until_service.rb
@@ -3,8 +3,6 @@
 module Expire
   # Calculate expiration until
   class KeepFirstOfIntervalUntilService < KeepBase
-    include Constants
-
     def initialize(now:, **args)
       super(args)
       @now = now
@@ -13,29 +11,27 @@ module Expire
     attr_reader :now
 
     def call
-      STEP_WIDTHS.each do |noun, adjective|
-        deadline = deadline_for(adjective)
-        next unless deadline
+      return unless deadline
 
-        message = "keep all #{adjective} until #{deadline}"
-
-        add_reasons_to_keep(backups.one_per(noun), message, deadline)
-      end
+      message = "keep all #{adjective} until #{deadline}"
+      add_reasons_to_keep(backups.one_per(noun), message) # , deadline)
     end
 
-    def deadline_for(adjective)
+    def amount
       rule = "#{adjective}_for"
-      amount = rules.send rule
+      rules.send rule
+    end
+
+    def deadline
       return unless amount
 
       num, duration = amount.split(/[^a-zA-Z0-9]/)
-
       now - num.to_i.send(duration)
     end
 
     private
 
-    def add_reasons_to_keep(thinned_out_backups, message, deadline)
+    def add_reasons_to_keep(thinned_out_backups, message)
       thinned_out_backups.each do |backup|
         backup.add_reason_to_keep(message) if backup >= deadline
       end

bb45a3f395928c72e75c5a64a725ad97614875c1
ossi <ossi@asgard.localdomain>
2020-05-14 21:02:42 +0200

    Refine KeepFirstOfIntervalUntilService

--- lib/expire/keep_first_of_interval_until_service.rb
+++ lib/expire/keep_first_of_interval_until_service.rb
@@ -5,36 +5,33 @@ module Expire
   class KeepFirstOfIntervalUntilService < KeepBase
     def initialize(now:, **args)
       super(args)
+      @memoized = {}
       @now = now
     end
 
     attr_reader :now
 
     def call
-      return unless deadline
+      deadline = calculate_deadline || return
 
       message = "keep all #{adjective} until #{deadline}"
-      add_reasons_to_keep(backups.one_per(noun), message) # , deadline)
+
+      backups.one_per(noun).each do |backup|
+        backup.add_reason_to_keep(message) if backup >= deadline
+      end
     end
 
-    def amount
+    def calculate_amount
       rule = "#{adjective}_for"
       rules.send rule
     end
 
-    def deadline
+    def calculate_deadline
+      amount = calculate_amount || return
       return unless amount
 
       num, duration = amount.split(/[^a-zA-Z0-9]/)
       now - num.to_i.send(duration)
-    end
-
-    private
-
-    def add_reasons_to_keep(thinned_out_backups, message)
-      thinned_out_backups.each do |backup|
-        backup.add_reason_to_keep(message) if backup >= deadline
-      end
     end
   end
 end

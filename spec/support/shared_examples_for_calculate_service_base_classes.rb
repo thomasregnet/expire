15cac1bdd27aa7b6ef652004ee3c0099af77eaea
ossi <ossi@asgard.localdomain>
2020-05-19 20:58:47 +0200

    Add CalculateServiceBase

---
+++ spec/support/shared_examples_for_calculate_service_base_classes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'a CalculateServiceBase class' do
+  it { should respond_to(:backups) }
+  it { should respond_to(:call) }
+  it { should respond_to(:rules) }
+end

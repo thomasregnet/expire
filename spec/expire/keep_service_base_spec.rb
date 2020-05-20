ad0e70d5659e0118990c19d783d45f9e2bc3f2d3
ossi <ossi@asgard.localdomain>
2020-05-20 20:05:44 +0200

    Remove KeepServiceBase

--- spec/expire/keep_service_base_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'support/shared_examples_for_keep_services'
-
-RSpec.describe Expire::KeepServiceBase do
-  it_behaves_like 'a keep service' do
-    let(:constructor_args) { {} }
-  end
-end

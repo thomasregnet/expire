fdd891ef850ad80c175ddf5b230fc0f34215e6a0
ossi <ossi@asgard.localdomain>
2020-06-11 10:50:40 +0200

    Add specs for BackupList#latest

--- spec/expire/backup_list_spec.rb
+++ spec/expire/backup_list_spec.rb
@@ -9,7 +9,54 @@ RSpec.describe Expire::BackupList do
     end
   end
 
-  describe '#latest'
+  describe '#latest' do
+    let(:backup_list) do
+      described_class.new(
+        [
+          Expire::Backup.new(
+            datetime: DateTime.new(1860, 5, 17, 12, 0, 0),
+            path:     :oldest
+          ),
+
+          Expire::Backup.new(
+            datetime: DateTime.new(1860, 5, 17, 12, 44, 0),
+            path:     :latest
+          ),
+          Expire::Backup.new(
+            datetime: DateTime.new(1860, 5, 17, 12, 36, 0),
+            path:     :middle
+          )
+        ]
+      )
+    end
+
+    context 'without parameter' do
+      it 'returns an Array with one element' do
+        expect(backup_list.latest.length).to eq(1)
+      end
+
+      it 'returns the latest backup' do
+        expect(backup_list.latest.last.path).to eq(:latest)
+      end
+    end
+
+    context 'with 2 as parameter' do
+      it 'returns an Array with one element' do
+        expect(backup_list.latest(2).length).to eq(2)
+      end
+
+      it 'returns the latest backup' do
+        paths = backup_list.latest(2).map(&:path)
+        expect(paths).to eq(%i[latest middle])
+      end
+    end
+
+    context 'with a parameter that exceeds the amount of backups' do
+      it 'returns an Array with the backups' do
+        expect(backup_list.latest(99).length).to eq(3)
+      end
+    end
+  end
 
   describe '#latest_one' do
     let(:backup_list) do

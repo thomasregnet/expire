ef4a44207b29d3cb98f7543da3509515b070efaa
ossi <ossi@asgard.localdomain>
2020-06-22 15:02:36 +0200

    Introduce Expire::SpacingRule

---
+++ lib/expire/spacing_rule.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Expire
+  # A rule with a spacing
+  class SpacingRule < SimpleRule
+    VALID_SPACINGS = %w[hour hours day days week weeks month months year years]
+
+    def initialize(spacing:, **args)
+      super(args)
+      raise ArgumentError, "#{spacing} is not a valid spacing" \
+        unless VALID_SPACINGS.include?(spacing)
+
+      @spacing = spacing
+    end
+
+    attr_reader :spacing
+  end
+end

c70e647e4f87c4a121dd5558fd6f23408fd13fe7
ossi <ossi@asgard.localdomain>
2020-06-22 12:40:16 +0200

    Remove from_now_one_per_unit rules

--- lib/expire/new_rules.rb
+++ lib/expire/new_rules.rb
@@ -15,10 +15,6 @@ module Expire
       "one_per_#{unit}_for".to_sym
     end.freeze
 
-    FROM_NOW_ONE_PER_UNIT_RULE_NAMES = STEP_NOUNS.map do |unit|
-      "from_now_one_per_#{unit}".to_sym
-    end.freeze
-
     FROM_NOW_ONE_PER_UNIT_FOR_RULE_NAMES = STEP_NOUNS.map do |unit|
       "from_now_one_per_#{unit}_for".to_sym
     end.freeze
@@ -27,7 +23,6 @@ module Expire
       'at_least',
       ONE_PER_UNIT_RULE_NAMES,
       ONE_PER_UNIT_FOR_RULE_NAMES,
-      FROM_NOW_ONE_PER_UNIT_RULE_NAMES,
       FROM_NOW_ONE_PER_UNIT_FOR_RULE_NAMES
     ].flatten.freeze
 
--- spec/expire/new_rules_spec.rb
+++ spec/expire/new_rules_spec.rb
@@ -13,12 +13,6 @@ RSpec.describe Expire::NewRules do
   it { should respond_to(:one_per_month_for) }
   it { should respond_to(:one_per_year_for) }
 
-  it { should respond_to(:from_now_one_per_hour) }
-  it { should respond_to(:from_now_one_per_day) }
-  it { should respond_to(:from_now_one_per_week) }
-  it { should respond_to(:from_now_one_per_month) }
-  it { should respond_to(:from_now_one_per_year) }
-
   it { should respond_to(:from_now_one_per_hour_for) }
   it { should respond_to(:from_now_one_per_day_for) }
   it { should respond_to(:from_now_one_per_week_for) }

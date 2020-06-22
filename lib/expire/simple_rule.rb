54f65973779b97d36b2b126d66e41f7543bf240b
ossi <ossi@asgard.localdomain>
2020-06-22 10:39:20 +0200

    Add SimpleRule.from_string

--- lib/expire/simple_rule.rb
+++ lib/expire/simple_rule.rb
@@ -3,25 +3,36 @@
 module Expire
   # A simple expire rule consists of the amount of backups to keep an a name
   class SimpleRule
+    class << self
+      def from_string(string, name:)
+        stripped = string.strip.downcase
+
+        amount = case stripped
+                 when 'all' then -1
+                 when 'none' then 0
+                 else
+                   amount_to_i(stripped, string, name)
+                 end
+
+        new(amount: amount, name: name)
+      end
+
+      private
+
+      def amount_to_i(amount, string, name)
+        Integer(amount)
+      rescue StandardError
+        raise ArgumentError, "#{string} is not a valid value for #{name}"
+      end
+    end
+
+    # private_class_method :amount_to_i
+
     def initialize(amount:, name:)
       @amount = amount
       @name   = name
-
-      validate_amount
     end
 
     attr_reader :amount, :name
-
-    private
-
-    # This method smells of :reek:DuplicateMethodCall
-    def validate_amount
-      @amount = Integer(amount)
-    rescue ArgumentError
-      raise ArgumentError, "#{amount} is not a valid value for #{name}"
-    rescue TypeError
-      amount ||= 'nil'
-      raise ArgumentError, "#{amount} is not a valid value for #{name}"
-    end
   end
 end
--- spec/expire/simple_rule_spec.rb
+++ spec/expire/simple_rule_spec.rb
@@ -16,25 +16,44 @@ RSpec.describe Expire::SimpleRule do
         expect(rule.amount).to eq(3)
       end
     end
+  end
 
-    context 'with a string that looks like an integer as value' do
-      it 'returns that integer' do
-        rule = described_class.new(amount: '3_000', name: 'my_rule')
-        expect(rule.amount).to eq(3_000)
+  describe '.from_string' do
+    context 'with a string representing a positive integer' do
+      let(:rule) { described_class.from_string(' 123 ', name: 'my_rule') }
+
+      it 'sets the right amount' do
+        expect(rule.amount).to eq(123)
+      end
+
+      it 'sets the name' do
+        expect(rule.name).to eq('my_rule')
       end
     end
 
-    context 'with nil as value' do
-      it 'raises an ArgumentError' do
-        expect { described_class.new(amount: nil, name: 'my_rule') }
-          .to raise_error ArgumentError, 'nil is not a valid value for my_rule'
+    context 'with the string "ALL"' do
+      let(:rule) { described_class.from_string(' ALL ', name: 'my_rule') }
+
+      it 'sets the amount to -1' do
+        expect(rule.amount).to eq(-1)
       end
     end
 
-    context 'with an invalid value' do
+    context 'with the string "NONE"' do
+      let(:rule) { described_class.from_string(' NONE ', name: 'my_rule') }
+
+      it 'sets the amount to 0' do
+        expect(rule.amount).to eq(0)
+      end
+    end
+
+    context 'with the string "Grimpflwurg"' do
       it 'raises an ArgumentError' do
-        expect { described_class.new(amount: 'bad', name: 'my_rule') }
-          .to raise_error ArgumentError, 'bad is not a valid value for my_rule'
+        expect { described_class.from_string('Grimpflwurg', name: 'my_rule') }
+          .to raise_error(
+            ArgumentError,
+            'Grimpflwurg is not a valid value for my_rule'
+          )
       end
     end
   end

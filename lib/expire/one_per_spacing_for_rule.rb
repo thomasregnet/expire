ca7834ebaf5ac882c0e2f516244083e30579c21e
ossi <ossi@asgard.localdomain>
2020-06-29 16:32:13 +0200

    Introduce Expire::OnePerSpacingFor

---
+++ lib/expire/one_per_spacing_for_rule.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Expire
+  # Hold backups for a period
+  class OnePerSpacingForRule < SpacingRuleBase
+    class << self
+      def from_string(string, **args)
+        stripped = string.strip.downcase
+
+        return new(amount: -1, spacing: 'year', unit: nil) if stripped == 'all'
+
+        match = stripped.match %r{
+          \A
+          ([0-9_]+)
+          [^0-9a-zA-Z]+
+          (hour|day|week|month|year)s?
+          \z
+        }x
+
+        raise ArgumentError, "#{string} is not a valid period" unless match
+
+        args[:amount] = Integer(match[1])
+        args[:unit]   = match[2]
+        new(args)
+      end
+    end
+
+    def initialize(unit:, **args)
+      super(args)
+
+      @unit = unit
+    end
+
+    attr_reader :unit
+  end
+end
---
+++ spec/expire/one_per_spacing_for_rule_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'support/shared_examples_for_rules'
+
+RSpec.describe Expire::OnePerSpacingForRule do
+  subject do
+    described_class.new(
+      amount:  1,
+      spacing: 'week',
+      unit:    'year'
+    )
+  end
+
+  it_behaves_like 'a rule'
+
+  it { should respond_to(:unit) }
+
+  describe '.from_string' do
+    context 'with the string "12_000 hour"' do
+      let(:rule) { described_class.from_string('12_000 hour', spacing: 'day') }
+
+      it 'has an amount of 12,000' do
+        expect(rule.amount).to eq(12_000)
+      end
+
+      it 'uses "hour" as unit' do
+        expect(rule.unit).to eq('hour')
+      end
+    end
+
+    context 'with the string "3 days"' do
+      let(:rule) { described_class.from_string('3 days', spacing: 'day') }
+
+      it 'has an amount of 3' do
+        expect(rule.amount).to eq(3)
+      end
+
+      it 'uses "day" as unit' do
+        expect(rule.unit).to eq('day')
+      end
+    end
+
+    context 'with the string "12 weeks"' do
+      let(:rule) { described_class.from_string('12 weeks', spacing: 'day') }
+
+      it 'has an amount of 12' do
+        expect(rule.amount).to eq(12)
+      end
+
+      it 'uses "week" as unit' do
+        expect(rule.unit).to eq('week')
+      end
+    end
+
+    context 'with the string "24.months"' do
+      let(:rule) { described_class.from_string('24.months', spacing: 'day') }
+
+      it 'has an amount of 24' do
+        expect(rule.amount).to eq(24)
+      end
+
+      it 'uses "month" as unit' do
+        expect(rule.unit).to eq('month')
+      end
+    end
+
+    context 'with the string "10&.%YEARS"' do
+      let(:rule) { described_class.from_string('10&.%YEARS', spacing: 'day') }
+
+      it 'has an amount of 10' do
+        expect(rule.amount).to eq(10)
+      end
+
+      it 'uses "year" as unit' do
+        expect(rule.unit).to eq('year')
+      end
+    end
+
+    context 'with the string "333 bad string"' do
+      it 'raises an ArgumentError' do
+        expect { described_class.from_string('333 bad string', spacing: 'day') }
+          .to raise_error(ArgumentError, '333 bad string is not a valid period')
+      end
+    end
+
+    context 'with the string "all"' do
+      let(:rule) { described_class.from_string('all', spacing: 'day') }
+
+      it 'has an amount of -1' do
+        expect(rule.amount).to eq(-1)
+      end
+
+      it 'has no unit' do
+        expect(rule.unit).to be_nil
+      end
+    end
+  end
+end

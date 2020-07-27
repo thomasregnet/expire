5f8ca9492d08c07601b6d4d3fdc9548bfcf8076e
ossi <ossi@asgard.localdomain>
2020-07-27 15:15:00 +0200

    Add Expire::AdjectiveRule

---
+++ lib/expire/adjective_rule.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Expire
+  # Base class for rules with an adjective in their name
+  class AdjectiveRule < Rule
+    include Constants
+
+    def spacing
+      match = class_name.downcase.match(/(hourly|daily|weekly|monthly|yearly)/)
+
+      return unless match
+
+      NOUN_FOR[match[1]]
+    end
+
+    private
+
+    def class_name
+      self.class.name
+    end
+  end
+end
---
+++ spec/expire/adjective_rule_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'support/shared_examples_for_rules'
+
+RSpec.describe Expire::AdjectiveRule do
+  subject { described_class.new(amount: 3) }
+
+  it_behaves_like 'a rule'
+
+  describe '#spacing' do
+    let(:instance) { described_class.new(amount: 3) }
+
+    context 'when the class-name does not contain a valid adjective' do
+      it 'returns nil' do
+        expect(instance.spacing).to be_nil
+      end
+    end
+
+    nouns_for = {
+      'hourly' => 'hour',
+      'daily' => 'day',
+      'weekly' => 'week',
+      'monthly' => 'month',
+      'yearly' => 'year'
+    }
+
+    nouns_for.each do |adjective, noun|
+      context "with a class-name containing \"#{adjective}\"" do
+        before do
+          allow(instance)
+            .to receive(:class_name)
+            .and_return("Hello::My#{adjective.capitalize}Rule")
+        end
+
+        it "returns \"#{noun}\"" do
+          expect(instance.spacing).to eq(noun)
+        end
+      end
+    end
+  end
+end

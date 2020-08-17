19448a22ccab2c8bd6c42e0f355c7531b662fbcc
ossi <ossi@asgard.localdomain>
2020-08-17 12:57:10 +0200

    Correct parameters for Commands::Remove#execute

--- lib/expire/commands/remove.rb
+++ lib/expire/commands/remove.rb
@@ -12,7 +12,7 @@ module Expire
 
       attr_reader :path
 
-      def execute(input: $stdin, output: $stdout)
+      def execute(output: $stdout)
         begin
           Expire.remove(path)
         rescue Errno::ENOENT => e

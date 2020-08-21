24bb66572d316fef48ef52282e904b2031d49e17
ossi <ossi@asgard.localdomain>
2020-08-21 08:54:32 +0200

    Remove commented out code

--- lib/expire/adjective_for_rule_base.rb
+++ lib/expire/adjective_for_rule_base.rb
@@ -16,33 +16,7 @@ module Expire
       'years' => 'yearly'
     }.freeze
 
-    # FROM_STRING_REGEX = /
-    # \A
-    # ([0-9_]+)
-    # [^0-9a-zA-Z]+
-    # (hour|day|week|month|year)s?
-    # \z
-    # /x.freeze
-
     PRIMARY_RANK = 30
-
-    # def self.from_string(string, **args)
-    #   stripped = string.strip.downcase
-    #   match = stripped.match FROM_STRING_REGEX
-    #   raise ArgumentError, "#{string} is not a valid period" unless match
-
-    #   amount = Integer(match[1])
-    #   unit = match[2]
-    #   new(args.merge({ amount: amount, unit: unit }))
-    # end
-
-    # def initialize(unit:, **args)
-    #   super(args)
-
-    #   @unit = unit
-    # end
-
-    # attr_reader :unit
 
     def apply(backups, _)
       super(backups, backups.newest)

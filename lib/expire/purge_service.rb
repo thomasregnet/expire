c2f68e96ccc37fb7e91ac2c70c59f964a2c78a3f
ossi <ossi@asgard.localdomain>
2020-08-28 11:23:19 +0200

    Refactor PurgeService

--- lib/expire/purge_service.rb
+++ lib/expire/purge_service.rb
@@ -17,10 +17,14 @@ module Expire
     def call
       raise NoRulesError, 'Will not purge without rules' unless rules.any?
 
-      if options[:purge_command]
-        purge_with_command
-      else
-        purge_without_command
+      annotaded_backup_list.each do |backup|
+        if backup.expired?
+          format.before_purge(backup)
+          purge_path(backup.path)
+          format.after_purge(backup)
+        else
+          format.on_keep(backup)
+        end
       end
     end
 
@@ -46,40 +50,26 @@ module Expire
         or raise ArgumentError, "unknown format \"#{wanted_format}\""
     end
 
-    def purge_without_command
-      annotaded_backup_list.each do |backup|
-        if backup.expired?
-          format.before_purge(backup)
-          FileUtils.rm_rf(backup.path)
-          format.after_purge(backup)
-        else
-          format.on_keep(backup)
-        end
-      end
-    end
-
-    def purge_with_command
-      annotaded_backup_list.each do |backup|
-        if backup.expired?
-          format.before_purge(backup)
-          system("#{options[:purge_command]} #{backup.path}")
-          format.after_purge(backup)
-        else
-          format.on_keep(backup)
-        end
-      end
-    end
-
-    def rules
-      @rules ||= merge_rules
-    end
-
     def merge_rules
       rules_file = options[:rules_file]
       file_rules = rules_file ? Rules.from_yaml(rules_file) : Rules.new
 
       option_rules = Rules.from_options(options.transform_keys(&:to_sym))
       file_rules.merge(option_rules)
+    end
+
+    def purge_path(path)
+      purge_command = options[:purge_command]
+
+      if purge_command
+        system("#{purge_command} #{path}")
+      else
+        FileUtils.rm_rf(path)
+      end
+    end
+
+    def rules
+      @rules ||= merge_rules
     end
   end
 end

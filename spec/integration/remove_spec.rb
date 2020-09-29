e413517ec88c94189f4805cfbe4b4a7e6944ddd8
thomasregnet <ossi@asgard.localdomain>
2020-09-29 13:21:38 +0200

    Fix rubocop issue in spec/integration/remove_spec

--- spec/integration/remove_spec.rb
+++ spec/integration/remove_spec.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe '`expire remove` command', type: :cli do
-  it 'executes `expire help remove` command successfully' do
-    output = `expire help remove`
-    expected_output = <<~OUT
+  let(:expected_output) do
+    <<~OUT
       Usage:
         expire remove
-      
+
       Options:
         -h, [--help], [--no-help]  # Display usage information
-      
+
       Command description...
     OUT
+  end
 
+  it 'executes `expire help remove` command successfully' do
+    output = `expire help remove`
     expect(output).to eq(expected_output)
   end
 end

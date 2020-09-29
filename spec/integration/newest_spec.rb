df7804418e8af426f03bcae2e5f54a0ecc9191fe
thomasregnet <ossi@asgard.localdomain>
2020-09-29 13:14:30 +0200

    Fix rubocop issue in spec/integration/newest_spec

--- spec/integration/newest_spec.rb
+++ spec/integration/newest_spec.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe '`expire newest` command', type: :cli do
-  it 'executes `expire help newest` command successfully' do
-    output = `expire help newest`
-    expected_output = <<~OUT
+  let(:expected_output) do
+    <<~OUT
       Usage:
         expire newest PATH
-      
+
       Options:
         -h, [--help], [--no-help]  # Display usage information
-      
+
       Show the newest backup
     OUT
+  end
 
+  it 'executes `expire help newest` command successfully' do
+    output = `expire help newest`
     expect(output).to eq(expected_output)
   end
 end

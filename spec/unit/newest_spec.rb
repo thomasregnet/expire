ed99388413538f520136a3d27d41b699cba346c8
thomasregnet <ossgewalt@gmail.com>
2021-01-18 21:36:41 +0100

    Add specs for "newest" with data from STDIN

--- spec/unit/newest_spec.rb
+++ spec/unit/newest_spec.rb
@@ -8,17 +8,28 @@ RSpec.describe Expire::Commands::Newest do
   let(:path) { 'tmp/aruba/backups' }
   let(:options) { {} }
 
-  before do
-    backup_path = Pathname.new('tmp/aruba/backups')
-    FileUtils.rm_rf(backup_path)
+  describe 'with an directory as input' do
+    before do
+      backup_path = Pathname.new('tmp/aruba/backups')
+      FileUtils.rm_rf(backup_path)
 
-    FileUtils.mkpath("#{backup_path}/1860-05-17T12_00_00")
-    FileUtils.mkpath("#{backup_path}/1860-05-17T13_00_00")
+      FileUtils.mkpath("#{backup_path}/1860-05-17T12_00_00")
+      FileUtils.mkpath("#{backup_path}/1860-05-17T13_00_00")
+    end
+
+    it 'executes `newest` command successfully' do
+      command = described_class.new(path, options)
+      command.execute(output: output)
+      expect(output.string).to eq("tmp/aruba/backups/1860-05-17T13_00_00\n")
+    end
   end
 
-  it 'executes `newest` command successfully' do
-    command = described_class.new(path, options)
-    command.execute(output: output)
-    expect(output.string).to eq("tmp/aruba/backups/1860-05-17T13_00_00\n")
+  describe 'with input from STDIN' do
+    it 'executes `newest` command successfully' do
+      $stdin = StringIO.new("/tsv/1860-05-17T12_00_00\n/tsv/1860-05-17T13_00_00\n")
+      command = described_class.new('-', options)
+      command.execute(output: output)
+      expect(output.string).to eq("/tsv/1860-05-17T13_00_00\n")
+    end
   end
 end

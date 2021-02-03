88b838242999714bc2002042ecc9e1e964cb2776
thomasregnet <ossgewalt@gmail.com>
2021-02-03 22:42:44 +0100

    Implement ReportBase#error

--- spec/expire/report_base_spec.rb
+++ spec/expire/report_base_spec.rb
@@ -7,6 +7,18 @@ RSpec.describe Expire::ReportBase do
   it_behaves_like 'a reporter'
   it_behaves_like 'a ReportBase descendant'
 
+  describe '#error' do
+    let(:message) { 'bad evil' }
+    let(:pastel) { spy }
+    let(:report) { described_class.new }
+
+    it 'calls pastel.red(message)' do
+      allow(report).to receive(:pastel).and_return(pastel)
+      report.error(message)
+      expect(pastel).to have_received(:red).with(message)
+    end
+  end
+
   describe '#pastel' do
     it 'returns an instance of Pastel::Delegator' do
       expect(described_class.new.pastel).to be_instance_of(Pastel::Delegator)

f90717be04c29010d9951345daf0bc19ae25e8ce
thomasregnet <ossgewalt@gmail.com>
2021-02-03 22:29:34 +0100

    Add test for ReportBase#pastel

--- spec/expire/report_base_spec.rb
+++ spec/expire/report_base_spec.rb
@@ -6,4 +6,10 @@ require 'support/shared_examples_for_report_base_descendants'
 RSpec.describe Expire::ReportBase do
   it_behaves_like 'a reporter'
   it_behaves_like 'a ReportBase descendant'
+
+  describe '#pastel' do
+    it 'returns an instance of Pastel::Delegator' do
+      expect(described_class.new.pastel).to be_instance_of(Pastel::Delegator)
+    end
+  end
 end
